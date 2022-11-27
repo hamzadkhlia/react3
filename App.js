82959dc2ef5e1328f25a3de9f070eb42d5165d57
hamzadkhlia <114502337+hamzadkhlia@users.noreply.github.com>
2022-11-27 22:01:01 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import'./App.css'
+import prfil from "./profil/the profile"
+
+const MyProfilePic = () =>
+ {return (  <img src="/hzd14.jpg" alt="" width="500" class="rounded-circle"/>   ); }; 
+export default MyProfilePic;
+
+const fullName = () => {
+  return (
+    <div>
+ hello my name is hamza dkhlia
+    </div>
+  )
+}
+
+export default profil
